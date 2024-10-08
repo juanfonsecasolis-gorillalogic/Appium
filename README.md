61ac1facdf67c4869fcca4baa2870d252b6e3bae
Juan Fonseca <juan.fonseca@gorillalogic.com>
2024-10-08 17:44:27 -0600

    Initial creation.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Appium
+
+2024 Juan M. Fonseca-Solís @ GorillaLogic
+
+## Description
 Exercises of course https://gorillalogic.udemy.com/course/the-complete-appium-course-for-ios-and-android
---
+++ demo/src/test/java/StartDriverSession.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class StartDriverSession {
+    
+    public static void main(String[] args)
+    {
+        ChromeOptions caps = new ChromeOptions();
+    }
+
+}
