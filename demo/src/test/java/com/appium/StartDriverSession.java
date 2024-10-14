2f5b912506a0871906fc43f45134b7d2c936a10f
Juan Fonseca <juan.fonseca@gorillalogic.com>
2024-10-14 16:52:32 -0600

    Finished PoC for iOS.

--- demo/src/test/java/com/appium/StartDriverSession.java
+++ demo/src/test/java/com/appium/StartDriverSession.java
@@ -1,13 +1,15 @@
+package com.appium;
+
 import java.io.File;
 import java.net.URL;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-
+import io.appium.java_client.ios.IOSDriver;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import dev.failsafe.internal.util.Assert;
 
 public class StartDriverSession {
     
@@ -17,10 +19,10 @@ public class StartDriverSession {
     public void setUp() throws Exception
     {
         ChromeOptions caps = new ChromeOptions();
-        caps.setCapability("appium:platformName", "iOS");
-        caps.setCapability("appium:deviceName", "iPhone 6");
+        caps.setCapability("platformName", "iOS");
         caps.setCapability("appium:automationName", "XCUITest");
-        caps.setCapability("appium:udid", "emulator-5554");
+        caps.setCapability("appium:deviceName", "iPhone 15");
+        caps.setCapability("appium:udid", "3B5EB227-9C4D-4EA8-8621-37544D56C1C9");
         String appPath = System.getProperty("user.dir") 
             + File.separator 
             + "src"
@@ -30,10 +32,9 @@ public class StartDriverSession {
             + "resources"
             + File.separator
             + "UIKitCatalog-iphonesimulator.app";
-        Reporter.log( "APP PATH: " + appPath, true );
-        caps.setCapability("app", appPath);
+        caps.setCapability("appium:app", appPath);
 
-        appiumDriver = new AndroidDriver(
+        appiumDriver = new IOSDriver(
             new URL("http://127.0.0.1:4723/"), 
             caps);
     }
@@ -46,7 +47,7 @@ public class StartDriverSession {
     @Test
     public void SimpleTest() throws InterruptedException
     {
-        Thread.sleep(1000);
+        Assert.isTrue(1==1, "1==1");
     }
 
 }
